327ec31b0b552f31413899930d8ccba48d196ed8
Bibek Chapagain <bibekchapagain27>
2020-01-25 20:49:00 +0545

    template literals in JS ES6

---
+++ index.js
@@ -0,0 +1,18 @@
+//Introduction to ES6___scrimba 
+
+//Tempelate Literals
+
+
+let firstName = 'Bibek';
+let lastName = 'Chapagain';
+
+//const fullName = `${firstName} ${lastName}`;
+
+let example = `${firstName}
+${lastName}
+`;
+
+console.log(example);
+document.getElementById('example').innerText = example;
+
+
