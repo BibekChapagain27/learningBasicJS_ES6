65acebb0958d1737dbcc6be42459293c537a83f9
Bibek Chapagain <bibekchapagain27>
2020-01-25 21:05:20 +0545

    destructuring objects in JS ES6

--- index.js
+++ index.js
@@ -3,16 +3,34 @@
 //Tempelate Literals
 
 
-let firstName = 'Bibek';
-let lastName = 'Chapagain';
+// let firstName = 'Bibek';
+// let lastName = 'Chapagain';
 
-//const fullName = `${firstName} ${lastName}`;
+// //const fullName = `${firstName} ${lastName}`;
 
-let example = `${firstName}
-${lastName}
-`;
+// let example = `${firstName}
+// ${lastName}
+// `;
 
-console.log(example);
-document.getElementById('example').innerText = example;
+// console.log(example);
+// document.getElementById('example').innerText = example; //Error Document is not defined
+
+
+//Destructuring Objects
+
+const personalInformation = {
+    firstName: 'Bibek',
+    lastName: 'Chapagain',
+    city: 'Kathmandu',
+    state: 'Texas',
+    zipCode: 73301
+};
+
+console.log(`${personalInformation.firstName} ${personalInformation.lastName} is from ${personalInformation.city}`);
+
+const {firstName: fn, lastName: ln, city: ci} = personalInformation;
+
+
+console.log(`${fn} Prasad ${ln} is from ${ci}`);
 
 
